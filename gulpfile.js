50d04dd825605ad76304c8dcd057d404cae6e627
Eric Baer <me@ericbaer.com>
2014-08-25 22:48:35 -0700

    Update gulp file to lint more src

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
+'use strict';
+
 var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs   = require('gulp-jscs');
 
+var src = [
+  './lib/*.js',
+  'index.js',
+  'gulpfile.js'
+];
+
 gulp.task('jshint', function() {
-  return gulp.src('./lib/*.js')
+  return gulp.src(src)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('jscs', function () {
-  return gulp.src('./lib/*.js')
+  return gulp.src(src)
     .pipe(jscs());
 });
 
