a4f7fa1192efb62c2b93868cc84e8372a0bae64f
Eric Baer <me@ericbaer.com>
2014-08-26 09:28:49 -0700

    Set up testing

---
+++ test/lib/util.spec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var chai = require('chai');
+var assert = chai.assert;
+
+var Promise = require('bluebird');
+var util = require('../../lib/util.js');
+
+describe('util.js', function(){
+  describe('getFileList', function() {
+    it('should return a promise when given a string or an Array', function() {
+      var promise = util.getFileList(__dirname + '/../fixtures/btoa.js');
+      assert(promise instanceof Promise);
+    });
+    it('should throw an exception when passed an invalid object');
+    it('should use resolve an array with a single file when passed a filename as a string');
+    it('should use resolve an array with a single file when passed filename as an string');
+    it('should use minimatch to resolve to an array of files when passed an array with strings containing stars');
+    it('should use minimatch to resolve to an array of files when passed a string with stars');
+  });
+
+  describe('getSupportedBrowserList', function() {
+    it('should return a promise when given a string or an Array')
+  });
+})
