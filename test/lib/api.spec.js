dc272e3307bd50118098e4ca4859bb6d1d008bd8
Eric Baer <me@ericbaer.com>
2014-08-27 14:42:14 -0700

    Add spec file for lib/api.js

---
+++ test/lib/api.spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var chai = require('chai');
+var assert = chai.assert;
+
+/* global -Promise */
+var Promise = require('bluebird');
+var cani = require('../../lib/api.js');
+
+describe('api.js', function () {
+  describe('getFileList', function () {
+    it('should return a promise that resolves to the results Array', function (done) {
+      var promise = cani(__dirname + '/../fixtures/**/*.js', {});
+      assert(promise instanceof Promise);
+
+      promise.then(function (badTokens) {
+        assert(badTokens instanceof Array);
+        done();
+      });
+    });
+
+    it('should call the callback function with the results Array as the first argument',
+      function (done) {
+        var promise = cani(__dirname + '/../fixtures/**/*.js', {}, function (badTokens) {
+          assert(badTokens instanceof Array);
+          done();
+        });
+      });
+  });
+});
