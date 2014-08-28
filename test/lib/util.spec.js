e6b326f370171ff538ddb704277d4bec6acec6c3
Eric Baer <me@ericbaer.com>
2014-08-28 00:20:46 -0700

    Add code for getSupportedBrowsers to return real data

--- test/lib/util.spec.js
+++ test/lib/util.spec.js
@@ -63,7 +63,51 @@ describe('util.js', function () {
       });
   });
 
-  describe('getSupportedBrowserList', function () {
-    it('should return a promise when given a string or an Array');
+  describe('getSupportedBrowsers', function () {
+    it('should return an Object', function () {
+      var supportedBrowsers = util.getSupportedBrowsers({});
+      assert(_.isObject(supportedBrowsers));
+    });
+
+    it('should ignore invalid browsers', function () {
+      var supportedBrowsers = util.getSupportedBrowsers({
+        'ie': ['6'],
+        'firefox': ['3.4'],
+        'prairieDog': ['1.0']
+      });
+      assert(_.isEqual(supportedBrowsers, {
+        'ie': ['6'],
+        'firefox': ['3.4']
+      }));
+    });
+
+    it('should ignore browsers with empty version lists', function () {
+      var supportedBrowsers = util.getSupportedBrowsers({
+        'ie': [],
+        'firefox': ['3.4']
+      });
+      assert(_.isEqual(supportedBrowsers, {
+        'firefox': ['3.4']
+      }));
+    });
+
+    it('should ignore browsers with version lists that are not Strings or Arrays', function () {
+      var supportedBrowsers = util.getSupportedBrowsers({
+        'ie': {},
+        'chrome': 34,
+        'firefox': ['3.4']
+      });
+      assert(_.isEqual(supportedBrowsers, {
+        'firefox': ['3.4']
+      }));
+    });
+
+    it('should return an Object with values of type Array', function () {
+      var supportedBrowsers = util.getSupportedBrowsers({
+        'ie': ['6'],
+        'firefox': ['3.4']
+      });
+      assert(_.isArray(supportedBrowsers['ie']));
+    });
   });
 });
