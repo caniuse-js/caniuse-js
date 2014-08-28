fe3ae76a14f5a66b411daceb080bcd2e526f8222
Eric Baer <me@ericbaer.com>
2014-08-28 01:24:00 -0700

    Analyze should consume the APIs promise array

--- test/lib/api.spec.js
+++ test/lib/api.spec.js
@@ -8,23 +8,21 @@ var Promise = require('bluebird');
 var cani = require('../../lib/api.js');
 
 describe('api.js', function () {
-  describe('getFileList', function () {
-    it('should return a promise that resolves to the results Array', function (done) {
-      var promise = cani(__dirname + '/../fixtures/**/*.js', {});
-      assert(promise instanceof Promise);
+  it('should return a promise that resolves to the results Array', function (done) {
+    var promise = cani(__dirname + '/../fixtures/**/*.js', {});
+    assert(promise instanceof Promise);
 
-      promise.then(function (badTokens) {
+    promise.then(function (badTokens) {
+      assert(badTokens instanceof Array);
+      done();
+    });
+  });
+
+  it('should call the callback function with the results Array as the first argument',
+    function (done) {
+      var promise = cani(__dirname + '/../fixtures/**/*.js', {}, function (badTokens) {
         assert(badTokens instanceof Array);
         done();
       });
     });
-
-    it('should call the callback function with the results Array as the first argument',
-      function (done) {
-        var promise = cani(__dirname + '/../fixtures/**/*.js', {}, function (badTokens) {
-          assert(badTokens instanceof Array);
-          done();
-        });
-      });
-  });
 });
